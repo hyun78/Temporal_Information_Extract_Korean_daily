50f9f0fadc01e57fb3d72e5d6e9c94f044956063
Hyeonho Song <hyun78@github.com>
2018-02-02 22:12:37 +0900

    left added

--- lstm.py
+++ lstm.py
@@ -15,7 +15,7 @@ import tensorflow as tf
 def _read_words(filename):
 	script_path = os.getcwd()
 	filepath = os.path.join(script_path,'database/{filename}'.format(filename=filename))
-	with tf.gfile.Gfile(filpath,"r") as f:
+	with tf.gfile.GFile(filepath,"r") as f:
 		return f.read().replace("\n"," <eos> ").split()
 
 
@@ -162,8 +162,7 @@ def data_type():
 	return tf.float16 if FLAGS.use_fp16 else tf.float32
 
 class PTBInput(object):
-  """input 데이터"""
-
+	"""input 데이터"""
 	def __init__(self, config, data, name=None):
 		self.batch_size = batch_size = config.batch_size
 		self.num_steps = num_steps = config.num_steps
@@ -198,7 +197,7 @@ class PTBModel(object):
 		
 		if is_training and config.keep_prob < 1:
 			def attn_cell():
-		    	return tf.contrib.rnn.DropoutWrapper(lstm_cell(), output_keep_prob=config.keep_prob)
+				return tf.contrib.rnn.DropoutWrapper(lstm_cell(), output_keep_prob=config.keep_prob)
 		cell = tf.contrib.rnn.MultiRNNCell([attn_cell() for _ in range(config.num_layers)], state_is_tuple=True)
 		self._initial_state = cell.zero_state(batch_size, data_type())
 
@@ -297,5 +296,156 @@ class SmallConfig(object):
 	batch_size = 20
 	vocab_size = 10000
 
+class MediumConfig(object):
+	"""--model flag가 medium일때의 설정값들"""
+	init_scale = 0.05
+	learning_rate = 1.0
+	max_grad_norm = 5
+	num_layers = 2
+	num_steps = 35
+	hidden_size = 650
+	max_epoch = 6
+	max_max_epoch = 39
+	keep_prob = 0.5
+	lr_decay = 0.8
+	batch_size = 20
+	vocab_size = 10000
 
+
+class LargeConfig(object):
+	"""--model flag가 large일때의 설정값들"""
+	init_scale = 0.04
+	learning_rate = 1.0
+	max_grad_norm = 10
+	num_layers = 2
+	num_steps = 35
+	hidden_size = 1500
+	max_epoch = 14
+	max_max_epoch = 55
+	keep_prob = 0.35
+	lr_decay = 1 / 1.15
+	batch_size = 20
+	vocab_size = 10000
+
+
+class TestConfig(object):
+	"""Tiny config, for testing."""
+	init_scale = 0.1
+	learning_rate = 1.0
+	max_grad_norm = 1
+	num_layers = 1
+	num_steps = 2
+	hidden_size = 2
+	max_epoch = 1
+	max_max_epoch = 1
+	keep_prob = 1.0
+	lr_decay = 0.5
+	batch_size = 20
+	vocab_size = 10000
+
+
+def run_epoch(session, model, eval_op=None, verbose=False):
+	"""Runs the model on the given data."""
+	start_time = time.time()
+	costs = 0.0
+	iters = 0
+	state = session.run(model.initial_state)
+
+	fetches = {
+		"cost": model.cost,
+		"final_state": model.final_state,
+	}
+	if eval_op is not None:
+		fetches["eval_op"] = eval_op
+
+	for step in range(model.input.epoch_size):
+		feed_dict = {}
+	for i, (c, h) in enumerate(model.initial_state):
+		feed_dict[c] = state[i].c
+		feed_dict[h] = state[i].h
+
+	vals = session.run(fetches, feed_dict)
+	cost = vals["cost"]
+	state = vals["final_state"]
+
+	costs += cost
+	iters += model.input.num_steps
+
+	if verbose and step % (model.input.epoch_size // 10) == 10:
+		print("%.3f perplexity: %.3f speed: %.0f wps" %
+			(step * 1.0 / model.input.epoch_size, np.exp(costs / iters),
+			iters * model.input.batch_size / (time.time() - start_time)))
+	return np.exp(costs / iters)
+
+
+def get_config():
+	if FLAGS.model == "small":
+		return SmallConfig()
+	elif FLAGS.model == "medium":
+		return MediumConfig()
+	elif FLAGS.model == "large":
+		return LargeConfig()
+	elif FLAGS.model == "test":
+		return TestConfig()
+	else:
+		raise ValueError("Invalid model: %s", FLAGS.model)
+
+
+def main(_):
+	if not FLAGS.data_path:
+		raise ValueError("Must set --data_path to PTB data directory")
+
+	raw_data = ptb_raw_data(FLAGS.data_path)
+	train_data, valid_data, test_data, _ = raw_data
+
+	config = get_config()
+	eval_config = get_config()
+	eval_config.batch_size = 1
+	eval_config.num_steps = 1
+
+	with tf.Graph().as_default():
+		initializer = tf.random_uniform_initializer(-config.init_scale,config.init_scale)
+
+	with tf.name_scope("Train"):
+		train_input = PTBInput(config=config, data=train_data, name="TrainInput")
+		with tf.variable_scope("Model", reuse=None, initializer=initializer):
+			m = PTBModel(is_training=True, config=config, input_=train_input)
+		tf.summary.scalar("Training Loss", m.cost)
+		tf.summary.scalar("Learning Rate", m.lr)
+
+	with tf.name_scope("Valid"):
+		valid_input = PTBInput(config=config, data=valid_data, name="ValidInput")
+		with tf.variable_scope("Model", reuse=True, initializer=initializer):
+			mvalid = PTBModel(is_training=False, config=config, input_=valid_input)
+		tf.summary.scalar("Validation Loss", mvalid.cost)
+
+	with tf.name_scope("Test"):
+		test_input = PTBInput(config=eval_config, data=test_data, name="TestInput")
+		with tf.variable_scope("Model", reuse=True, initializer=initializer):
+			mtest = PTBModel(is_training=False, config=eval_config,input_=test_input)
+	
+	sv = tf.train.Supervisor(logdir=FLAGS.save_path)
+	
+	with sv.managed_session() as session:
+		for i in range(config.max_max_epoch):
+			lr_decay = config.lr_decay ** max(i + 1 - config.max_epoch, 0.0)
+			m.assign_lr(session, config.learning_rate * lr_decay)
+
+			print("Epoch: %d Learning rate: %.3f" % (i + 1, session.run(m.lr)))
+			train_perplexity = run_epoch(session, m, eval_op=m.train_op,
+			                             verbose=True)
+			print("Epoch: %d Train Perplexity: %.3f" % (i + 1, train_perplexity))
+			valid_perplexity = run_epoch(session, mvalid)
+			print("Epoch: %d Valid Perplexity: %.3f" % (i + 1, valid_perplexity))
+
+		test_perplexity = run_epoch(session, mtest)
+		print("Test Perplexity: %.3f" % test_perplexity)
+
+		if FLAGS.save_path:
+			print("Saving model to %s." % FLAGS.save_path)
+			sv.saver.save(session, FLAGS.save_path, global_step=sv.global_step)
+
+
+if __name__ == "__main__":
+	tf.app.run()
 
