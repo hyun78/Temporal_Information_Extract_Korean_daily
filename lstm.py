456cb7638b4abf5a1b56625867b8b0905cbd3e51
Hyeonho Song <hyun78@github.com>
2018-02-06 13:12:41 +0900

    konlpy config

--- lstm.py
+++ lstm.py
@@ -350,16 +350,16 @@ class KonlpyConfig(object):
 	""" korean의 경우 config"""
 	init_scale = 0.1
 	learning_rate = 1.0
-	max_grad_norm = 1
-	num_layers = 1
-	num_steps = 2
-	hidden_size = 2
-	max_epoch = 1
-	max_max_epoch = 1
+	max_grad_norm = 5
+	num_layers = 2
+	num_steps = 20
+	hidden_size = 200
+	max_epoch = 4
+	max_max_epoch = 13
 	keep_prob = 1.0
 	lr_decay = 0.5
 	batch_size = 20
-	vocab_size = 13840767
+	vocab_size = 10000
 
 def run_epoch(session, model, eval_op=None, verbose=False):
 	"""Runs the model on the given data."""
