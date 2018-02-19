0b59c9e9f30998495d63e066094a48fbcf90fb9e
Hyeonho Song <hyun78@github.com>
2018-02-19 15:38:29 +0900

    commented line roll back

--- lstm_korean.py
+++ lstm_korean.py
@@ -1,4 +1,6 @@
+
 # LSTM model
+# run:  python lstm_korean.py --model=small --data_path=bestiz_maksed_parsed
 import collections
 import os
 import sys, random
@@ -115,7 +117,7 @@ def tokenize_data(filename,time_flag=True):
 	return
 
 #tokenize -> preprocessing
-#ex : refine_data('testfolder/raw','bestiz.train.txt',is_dir=True)
+#ex : refine_data('testfolder/raw','bestiz_masked_parsed.train.txt',is_dir=True)
 def refine_data(filepath,vocab_name,is_dir=False):
 	if is_dir:
 		filenames = os.listdir('database/'+filepath)
@@ -366,15 +368,15 @@ import time
 import numpy as np
 
 #import reader 한 파일에 우겨넣었다.
-# flags = tf.flags
-# logging = tf.logging
+flags = tf.flags
+logging = tf.logging
 
-# flags.DEFINE_string("model", "small","A type of model. Possible options are: small, medium, large.")
-# flags.DEFINE_string("data_path", None,"Where the training/test data is stored.")
-# flags.DEFINE_string("save_path", None,"Model output directory.")
-# flags.DEFINE_string("vocab_path", None,"Vocabulary path.")
-# flags.DEFINE_bool("use_fp16", False,"Train using 16-bit floats instead of 32bit floats")
-# FLAGS = flags.FLAGS
+flags.DEFINE_string("model", "small","A type of model. Possible options are: small, medium, large.")
+flags.DEFINE_string("data_path", None,"Where the training/test data is stored.")
+flags.DEFINE_string("save_path", None,"Model output directory.")
+flags.DEFINE_string("vocab_path", None,"Vocabulary path.")
+flags.DEFINE_bool("use_fp16", False,"Train using 16-bit floats instead of 32bit floats")
+FLAGS = flags.FLAGS
 
 def data_type():
 	return tf.float16 if FLAGS.use_fp16 else tf.float32
